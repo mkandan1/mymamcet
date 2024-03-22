7e85f6b991e9380a9bb1aa0a327643ebba1dff6f
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2024-03-22 21:08:15 +0530

    fix: login page route

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,7 @@ function App() {
             <Route path='/#/' element={<Login />} />
           </Routes> :
           <Routes>
+            <Route path='/' element={<Login />} />
             <Route path='/login' element={<Login />} />
             <Route path='/*' element={<Login />} />
           </Routes>
